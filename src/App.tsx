45b2071912d03c203e5c8ceb9f95ab21003ed2e0
RyannKim327 <rksesgundo123@gmail.com>
2025-03-31 16:04:44 +0800

    03-31-25 16:04

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-
-
 function App() {
-  
   return (
-    <div className="w-dvw h-dvh">
-      <h1>Hello</h1>
+    <div className="w-dvw h-dvh dark:text-white dark:bg-linear-to-b dark:from-[#141E30] dark:to-[#141E30]">
+      <header className="flex flex-col justify-center items-center h-full">
+        <h1 className="text-emerald-200 text-3xl">Welcome to Capital K</h1>
+        <h3 className="text-emerald-500">The home of students and alumni</h3>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
